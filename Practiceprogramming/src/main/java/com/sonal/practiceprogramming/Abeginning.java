f85c8d459e043c8dd8cbfbca9b88fdfb35a0c2f2
Sonalrana27 <87694142+Sonalrana27@users.noreply.github.com>
2021-08-02 22:39:27 -0400

    practice program for beginning

---
+++ Practiceprogramming/src/main/java/com/sonal/practiceprogramming/Abeginning.java
@@ -0,0 +1,13 @@
+package com.sonal.practiceprogramming;
+public class Abeginning
+{
+public static void main(String[] args)
+{
+System.out.println("hello world");
+System.out.println("hello to Software Guild");
+System.out.println("i started my programming in java");
+System.out.println("first program is helloworld1");
+System.out.println("i compile,run & show all my words");
+System.out.println("the console type all my brilliant words");
+}
+}
