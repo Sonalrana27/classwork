047ae135a99f7419ca4a587ef6fee89b77f1af5f
Sonalrana27 <87694142+Sonalrana27@users.noreply.github.com>
2021-08-02 23:17:48 -0400

    all math operators program

---
+++ Practiceprogramming/src/main/java/com/sonal/practiceprogramming/allmaths.java
@@ -0,0 +1,17 @@
+package com.sonal.practiceprogramming;
+public class allmaths
+{
+public static void main(String[] args)
+{
+System.out.println("1+2 is..");
+System.out.println(1+2);
+System.out.println("42001 modulus 5 is");
+System.out.println(42001%5);
+System.out.println("5565.0 divided by 22.0 is");
+System.out.println(5565.0/22.0);
+System.out.println("4 is greater than -1");
+System.out.println(4>-1);
+System.out.println(22*3-1+4*6-9);
+System.out.println(67>4*5);
+}
+}
