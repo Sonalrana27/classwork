82ba49887358de028f44865074e4258c67655a8b
Sonalrana27 <87694142+Sonalrana27@users.noreply.github.com>
2021-08-02 22:58:03 -0400

    updated Gutenberg program

--- Practiceprogramming/src/main/java/com/sonal/practiceprogramming/projectGutenberg.java
+++ Practiceprogramming/src/main/java/com/sonal/practiceprogramming/projectGutenberg.java
@@ -3,12 +3,9 @@ public class projectGutenberg
 {
 public static void main(String[] args)
 {
-System.out.println("Did you know that in 1440 or thereabout,");
-System.out.println("Johnnes Gutenberg invented the printing press");
-System .out.println("He started as a goldsmith");
-System.out.println("His invention made it easy to print and");
-System.out.println("Distribute books to anyone who wants one");
-System.out.println("We are line a modern Gutenberg");
-System.out.println("printing vast amounts to wainting console with ease");
+System.out.print("Did you know that in 1440 or thereabout,Johnnes Gutenberg invented the printing press");
+System .out.print("He started as a goldsmith");
+System.out.print("His invention made it easy to print and Distribute books to anyone who wants one");
+System.out.print("We are line a modern Gutenberg,printing vast amounts to wainting console with ease");
 }
 }
