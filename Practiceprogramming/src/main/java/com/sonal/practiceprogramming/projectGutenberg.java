77297b25160efbe45fb1483a1b7068d83a4c5b01
Sonalrana27 <87694142+Sonalrana27@users.noreply.github.com>
2021-08-02 22:52:33 -0400

    run original Gutenberg program

---
+++ Practiceprogramming/src/main/java/com/sonal/practiceprogramming/projectGutenberg.java
@@ -0,0 +1,14 @@
+package com.sonal.practiceprogramming;
+public class projectGutenberg
+{
+public static void main(String[] args)
+{
+System.out.println("Did you know that in 1440 or thereabout,");
+System.out.println("Johnnes Gutenberg invented the printing press");
+System .out.println("He started as a goldsmith");
+System.out.println("His invention made it easy to print and");
+System.out.println("Distribute books to anyone who wants one");
+System.out.println("We are line a modern Gutenberg");
+System.out.println("printing vast amounts to wainting console with ease");
+}
+}
