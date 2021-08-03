3628736869f1ef180ae10acc3a8e3bfdb1d49958
Sonalrana27 <87694142+Sonalrana27@users.noreply.github.com>
2021-08-02 23:41:06 -0400

    A bucket program

---
+++ Practiceprogramming/src/main/java/com/sonal/practiceprogramming/InAbucket.java
@@ -0,0 +1,20 @@
+package com.sonal.practiceprogramming;
+public class InAbucket
+{
+public static void main(String[] args)
+{
+String walrus;
+double pieeaten;
+float weightofteacuppig;
+int grainsofsand;
+ 
+walrus= "sir lorey jakin III";
+pieeaten=42.1;
+
+System.out.println("meet me pet walrus "+walrus);
+System.out.println("he eaten this many pie");
+System.out.println(pieeaten);
+
+
+}
+}
