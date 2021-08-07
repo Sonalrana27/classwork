5910f07b7f615b0a4561408d8d58b97b1697c752
Sonalrana27 <87694142+Sonalrana27@users.noreply.github.com>
2021-08-07 11:13:35 -0400

    Life in Movies

---
+++ Practiceprogramming/src/main/java/com/sonal/practiceprogramming/flowcontrol/yourLifeInMovies.java
@@ -0,0 +1,25 @@
+package com.sonal.practiceprogramming.flowcontrol;
+public class yourLifeInMovies
+{
+public static void main(String[] args)
+{
+int a=1985;
+
+if(a<2005)
+{
+System.out.println("Pixar's Up");
+}
+if ((a>1985) && (a<2005))
+{
+System.out.println("Space jam came out");
+}
+if ((a>1975)&&(a<1985))
+{
+System.out.println("original Jurraisic park releases");
+}
+if((a>1965)&&(a<1975))
+{
+System.out.println("mask TV series has been arriveed for almost half a century");
+}
+}
+}
