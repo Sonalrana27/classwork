6be2b29a4290a0c71c6272418129354a1593850e
Sonalrana27 <87694142+Sonalrana27@users.noreply.github.com>
2021-08-07 10:43:40 -0400

    Guess me Program

---
+++ Practiceprogramming/src/main/java/com/sonal/practiceprogramming/flowcontrol/guessme.java
@@ -0,0 +1,22 @@
+package com.sonal.practiceprogramming.flowcontrol;
+
+public class guessme
+{
+public static void main(String[] args)
+{
+int a=27;
+
+if(a==27)
+{
+System.out.println("wow,nice guess");
+ if(a<27)
+{
+System.out.println("ha,nice try too low");
+}
+if(a>27)
+{
+System.out.println("too bad,way too high");
+}
+}
+}
+}
