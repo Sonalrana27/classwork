8718c45e87f9076748ad6c9b1b7a983efe37075e
Sonalrana27 <87694142+Sonalrana27@users.noreply.github.com>
2021-08-08 15:00:10 -0400

    little chaos changed program
    when i change number different result arrived

--- Practiceprogramming/src/main/java/com/sonal/foundations/random/Littlechaos.java
+++ Practiceprogramming/src/main/java/com/sonal/foundations/random/Littlechaos.java
@@ -8,14 +8,14 @@ Random rm=new Random();
 System.out.println("random can make integer"+rm.nextInt());
 System.out.println("or a Double"+rm.nextDouble());
 System.out.println("or even a boolean"+rm.nextBoolean());
-int num=rm.nextInt(100);
+int num=rm.nextInt(50);//changed number from 100 to50
 System.out.println("you can store randomized result"+num);
 System.out.println("and you use them over and over again" +  num +","  +num);
-System.out.println("here are some bunch of random number 0-100");
-System.out.println(rm.nextInt(101)+",");
-System.out.println(rm.nextInt(101)+",");
-System.out.println(rm.nextInt(101)+",");
-System.out.println(rm.nextInt(101));
+System.out.println("here are some bunch of random number 0-50");
+System.out.println(rm.nextInt(51)+",");//this will show result 39
+System.out.println(rm.nextInt(51)+",");//result is 40
+System.out.println(rm.nextInt(51)+",");//result is 26
+System.out.println(rm.nextInt(51));//result is 39
 }
 
 }
