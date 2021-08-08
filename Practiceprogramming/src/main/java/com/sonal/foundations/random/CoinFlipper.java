607e9f40ca664f87a647a070e029806f9df715fe
Sonalrana27 <87694142+Sonalrana27@users.noreply.github.com>
2021-08-08 16:52:12 -0400

    coin flipper program

---
+++ Practiceprogramming/src/main/java/com/sonal/foundations/random/CoinFlipper.java
@@ -0,0 +1,13 @@
+package com.sonal.foundations.random;
+import java.util.Random;
+public class CoinFlipper
+{
+public static void main(String[] args)
+{
+Random rm=new Random(10);
+System.out.println("time to flip the coin");
+System.out.println("tails");
+
+
+}
+}
