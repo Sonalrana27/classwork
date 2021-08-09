4274929ac9b038d217f0009b5f7af54b3eb8cd4c
Sonalrana27 <87694142+Sonalrana27@users.noreply.github.com>
2021-08-08 22:11:47 -0400

    Stay positive program

---
+++ Practiceprogramming/src/main/java/com/sonal/practiceprogramming/flowcontrol/whiles/staypositive.java
@@ -0,0 +1,19 @@
+package com.sonal.practiceprogramming.flowcontrol.whiles;
+public class staypositive
+{
+public static void main(String[] args)
+{
+int n=10;
+System.out.println("counting down" );
+
+
+while(n>=0)
+
+{
+System.out.println(+n);
+n--;
+}
+
+System.out.println("blast off");
+}
+}
