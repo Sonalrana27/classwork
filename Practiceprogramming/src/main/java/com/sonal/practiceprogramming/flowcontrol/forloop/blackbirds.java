962ddcaf1c3eeba33c43ea7c3183539078d57a2d
Sonalrana27 <87694142+Sonalrana27@users.noreply.github.com>
2021-08-09 21:12:31 -0400

    black birds program using for loop

---
+++ Practiceprogramming/src/main/java/com/sonal/practiceprogramming/flowcontrol/forloop/blackbirds.java
@@ -0,0 +1,14 @@
+package com.sonal.practiceprogramming.flowcontrol.forloop;
+public class blackbirds
+{
+public static void main(String[] args)
+{
+int birdpie=0;
+for(int i=0;i<20;i++)
+{
+System.out.println("birds"+i+"goes into pie");
+birdpie++;
+}
+System.out.println("ther are"+birdpie+"birds in there");
+}
+}
