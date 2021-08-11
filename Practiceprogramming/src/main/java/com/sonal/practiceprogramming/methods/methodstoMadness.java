85d73e0914fd1e677dec7564e18bbd4331373d17
Sonalrana27 <87694142+Sonalrana27@users.noreply.github.com>
2021-08-11 03:20:48 -0400

    methods to madness

---
+++ Practiceprogramming/src/main/java/com/sonal/practiceprogramming/methods/methodstoMadness.java
@@ -0,0 +1,19 @@
+package com.sonal.practiceprogramming.methods;
+public class methodstoMadness
+{
+public static void main(String[] args)
+{
+eatme();
+drinkme();
+}
+static void eatme()
+{
+System.out.println("Don't want to entered in Mad people,alice remarked");
+System.out.println("oh,you can't helpsaid the cal");
+}
+static void drinkme()
+{
+System.out.println("How do you know i am mad");
+System.out.println("you wouldn't have to come here");
+}
+}
