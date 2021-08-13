cb547a3589897f744e6e662ba2829ead28647fa4
Sonalrana27 <87694142+Sonalrana27@users.noreply.github.com>
2021-08-13 10:11:04 -0400

    lucky Seven program
    please ckeck and suggest i m not sure this code is perfect

---
+++ Practiceprogramming/src/main/java/com/sonal/practiceprogramming/LuckySevens.java
@@ -0,0 +1,36 @@
+package com.sonal.practiceprogramming;
+import java.util.Scanner;
+import java.util.Random;
+public class LuckySevens
+{
+public static void main(String[] args)
+{
+System.out.println("How many dollar do you have");
+Scanner sc=new Scanner(System.in);
+double dollor=sc.nextDouble();
+System.out.println(+dollor);
+
+System.out.println("you can roll dice");
+Random rm=new Random();
+int rolls=rm.nextInt(10);
+System.out.println(""+rolls);
+for(int i=1;i<rolls;i++)
+{
+int dice=rm.nextInt(6);
+for(int j=0;j<=dice;j++)
+{
+int sum=dice+j;
+System.out.println("sum of dies"+sum);
+
+if(sum==7)
+{
+System.out.println("you win $4");
+}
+else
+{
+System.out.println("you lose $1");
+}}
+}
+}
+
+}
