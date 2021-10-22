dd1ae875616c9f6c5c6dc5f749e86deb884fba42
Sonalrana27 <87694142+Sonalrana27@users.noreply.github.com>
2021-10-22 12:17:16 -0400

    app

--- DVDLibrary/src/main/java/com/sonal/dvdlibrary/ui/DVDLibraryView.java
+++ DVDLibrary/src/main/java/com/sonal/dvdlibrary/ui/DVDLibraryView.java
@@ -21,10 +21,10 @@ public class DVDLibraryView {
 
     public int printMenuAndGetSelection() {
         io.print("Main Menu");
-        io.print("1. List Students");
-        io.print("2. Create New Student");
-        io.print("3. View a Student");
-        io.print("4. Remove a Student");
+        io.print("1. List DVD");
+        io.print("2. Create New DVD");
+        io.print("3. View a DVD");
+        io.print("4. Remove a DVD");
         io.print("5. Exit");
 
         return io.readInt("Please select from the above choices.", 1, 5);
